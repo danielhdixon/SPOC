55a3a331d0ef794a75436167e3197d60fdc56367
Daniel H Dixon <115487137+danielhdixon@users.noreply.github.com>
2024-05-04 13:04:20 -0400

    Add files via upload

---
+++ SK_messages_vpublic.py
@@ -0,0 +1,86 @@
+author = 'DHDixon'
+email = 'ddixon49@gsu.edu'
+"""
+9/12/2021
+DHDixon
+Fallout 4 Messages texts to modified texts with word count
+
+"""
+import re
+import glob
+
+
+tab = "\t"
+sep = "\n"
+
+blanks = ('', ' ', sep, tab, '---')
+parentheses = ('(' or ')')
+
+unique_lines = []
+word_counts_list = []
+
+path = r"D:\Dropbox\0_python_working_directory\dissertation_python_wd\O_GAME_CORPUS\SK\raw\immersion_text" \
+       r"\SK_MessageExport_8_20" + '\\'
+folder_out = r'D:\Dropbox\0_python_working_directory\dissertation_python_wd\O_GAME_CORPUS\SK\sorted\written' \
+             r'\tutorial_text_v2\messages' + '\\'
+all_book_dict = {}
+
+# READ IN
+for file in glob.glob(path + '*.txt'):
+    # print(file)
+    with open(file, encoding='utf-8', errors='ignore') as file_in:
+        book_name = file.split('\\')[-1]
+
+        # Separate to lines
+        text = file_in.read()
+        lines = text.split(sep)
+        for line in lines:
+            # THIS IS HOW IT WORKS to not get blanks
+            if line not in blanks:
+
+                # Change square brackets to angled
+                line = re.sub('\\[', '<', line)
+                line = re.sub(']', '>', line)
+                unique_lines.append(line)
+            else:
+                print(line)
+                print('ABOVE IN BLANKS')
+
+        if book_name not in all_book_dict:
+            all_book_dict[book_name] = []
+        for line in unique_lines:
+            all_book_dict[book_name].append(line)
+        unique_lines = []
+
+book_count = 0
+for book in all_book_dict:
+    print(book)
+    word_count = 0
+    for line in all_book_dict[book]:
+        line = re.sub('<[^>]*>', '', line)
+        if line not in blanks:
+            words = line.split(' ')
+            word_count += len(words)
+
+    if word_count > 0:
+        word_counts_list.append(word_count)
+
+        # WRITE OUT
+        book_count += 1
+        file_out = open(folder_out + str(book_count) + '_' + book, 'w+', encoding='utf-8')
+
+        file_out.write('<' + book + '>' + sep)
+        file_out.write('<WORD COUNT: ' + str(word_count) + '>' + sep + sep)
+        for line in all_book_dict[book]:
+
+            # if line not in blanks:
+            print(line)
+            file_out.write(line + sep)
+print('TOTAL WORDS')
+print(sum(word_counts_list))
+
+
+
+
+
+
